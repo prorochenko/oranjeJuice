35069b65355a4086de81cc87cac5b849a76d2343
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-12-01 12:53:32 +0200

    edited

--- src/components/Presence/Presence.jsx
+++ src/components/Presence/Presence.jsx
@@ -1,28 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import scss from './Presence.module.scss';
 import { Element } from 'react-scroll';
 import Chart from 'react-google-charts';
 
-import React from 'react';
-
-const geoData = [
-  ['Country', 'Presence from', 'Partners'],
-  ['Germany', new Date(2012, 3, 13), 3],
-  ['United States', 2021, 4],
-  ['Brazil', 2020, 5],
-  ['Canada', 2019, 5],
-  ['France', 2018, 2],
-  ['Ukraine', 2020, 1],
-];
-
-const options = {
-  colorAxis: { colors: ['#F1AE00', '#F1AE00'] },
-  datalessRegionColor: 'white',
-  legend: 'none',
-  backgroundColor: '#FFF8EA',
-  tooltip: { textStyle: { color: '#F1AE00', fontSize: 16 } },
-};
-
 export const Presence = () => {
+  const [chartKey, setChartKey] = useState(0);
+
+  const handleResize = () => {
+    setChartKey(prevKey => prevKey + 1);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const geoData = [
+    ['Country', 'Presence from', 'Partners'],
+    ['Germany', new Date(2012, 3, 13), 3],
+    ['United States', 2021, 4],
+    ['Brazil', 2020, 5],
+    ['Canada', 2019, 5],
+    ['France', 2018, 2],
+    ['Ukraine', 2020, 1],
+  ];
+
+  const options = {
+    colorAxis: { colors: ['#F1AE00', '#F1AE00'] },
+    datalessRegionColor: 'white',
+    legend: 'none',
+    backgroundColor: '#FFF8EA',
+    tooltip: { textStyle: { color: '#F1AE00', fontSize: 16 } },
+  };
+
   return (
     <Element name="presence" className={scss.presence}>
       <h2 className={scss.presence__title}>Our presence</h2>
@@ -34,13 +46,12 @@ export const Presence = () => {
       </p>
 
       <Chart
+        key={chartKey}
         width={'100%'}
         height={'100%'}
         chartType="GeoChart"
         data={geoData}
         options={options}
-        // Note: you will need to get a mapsApiKey for your project.
-        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
         mapsApiKey="YOUR_MAP_API_KEY_GOES_HERE"
         rootProps={{ 'data-testid': '1' }}
       />
